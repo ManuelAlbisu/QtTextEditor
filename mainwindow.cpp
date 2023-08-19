e0d661b599a0291bec2b56e95ffbb4127600b310
Manuel Albisu <malbi005@fiu.edu>
2023-08-19 12:45:38 -0400

    Implemented menu bar, tool bar, and context menu.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     createActions();
     createMenus();
+    createToolBars();
+    createContextMenu();
 }
 
 MainWindow::~MainWindow()
@@ -18,31 +20,133 @@ MainWindow::~MainWindow()
 void MainWindow::createActions()
 {
     // New file action
-    m_newFileAction = new QAction("&New File", this);
+    m_newFileAction = new QAction("&New File...", this);
     m_newFileAction->setIcon(QIcon(":/images/new.png"));
     m_newFileAction->setShortcut(QKeySequence::New);
-    m_newFileAction->setStatusTip("Create a new file");
+    m_newFileAction->setStatusTip("Create a new file.");
     connect(m_newFileAction, SIGNAL(triggered()), this, SLOT(newFile()));
 
+    // Open file action
+    m_openFileAction = new QAction("&Open File...", this);
+    m_openFileAction->setIcon(QIcon(":/images/new.png"));
+    m_openFileAction->setShortcut(QKeySequence::Open);
+    m_openFileAction->setStatusTip("Open a file.");
+    connect(m_openFileAction, SIGNAL(triggered()), this, SLOT(openFile()));
+
+    // TODO: Save action
+
     // Save as action
-    m_saveAsAction = new QAction("&Save as...", this);
+    m_saveAsAction = new QAction("&Save As...", this);
     m_saveAsAction->setIcon(QIcon(":/images/save-as.png"));
     m_saveAsAction->setShortcut(QKeySequence::SaveAs);
     m_saveAsAction->setStatusTip("Save file as...");
     connect(m_saveAsAction, SIGNAL(triggered()), this, SLOT(saveAs()));
+
+    // TODO: Open recent file action
+
+    // Exit application action
+    m_exitAction = new QAction("&Exit", this);
+    m_exitAction->setIcon(QIcon(":/images/exit.png"));
+    m_exitAction->setShortcut(QKeySequence::Close);
+    m_exitAction->setStatusTip("Exit the application.");
+    connect(m_exitAction, SIGNAL(triggered()), this, SLOT(exit()));
+
+    // Copy to clipboard action
+    m_copyAction = new QAction("&Copy", this);
+    m_copyAction->setIcon(QIcon(":images/copy.png"));
+    m_copyAction->setShortcut(QKeySequence::Copy);
+    m_copyAction->setStatusTip("Copy selected text to clipboard.");
+    connect(m_copyAction, SIGNAL(triggered()), this, SLOT(copyToClipboard()));
+
+    // Paste from clipboard action
+    m_pasteAction = new QAction("&Paste", this);
+    m_pasteAction->setIcon(QIcon(":images/paste.png"));
+    m_pasteAction->setShortcut(QKeySequence::Paste);
+    m_pasteAction->setStatusTip("Paste text from clipboard.");
+    connect(m_pasteAction, SIGNAL(triggered()), this, SLOT(pasteFromClipboard()));
+
+    // Cut to clipboard action
+    m_cutAction = new QAction("&Cut", this);
+    m_cutAction->setIcon(QIcon(":images/cut.png"));
+    m_cutAction->setShortcut(QKeySequence::Cut);
+    m_cutAction->setStatusTip("Cut selected text to clipboard.");
+    connect(m_cutAction, SIGNAL(triggered()), this, SLOT(cutToClipboard()));
+
+    // Undo action
+    m_undoAction = new QAction("&Undo", this);
+    m_undoAction->setIcon(QIcon(":images/undo.png"));
+    m_undoAction->setShortcut(QKeySequence::Undo);
+    m_undoAction->setStatusTip("Undo previous action.");
+    connect(m_undoAction, SIGNAL(triggered()), this, SLOT(undo()));
+
+    // Redo action
+    m_redoAction = new QAction("&Redo", this);
+    m_redoAction->setIcon(QIcon(":images/redo.png"));
+    m_redoAction->setShortcut(QKeySequence::Redo);
+    m_redoAction->setStatusTip("Redo previous action.");
+    connect(m_redoAction, SIGNAL(triggered()), this, SLOT(redo()));
 }
 
 // Create menu bar
 void MainWindow::createMenus()
 {
+    // File menu
     m_fileMenu = menuBar()->addMenu("&File");
     m_fileMenu->addAction(m_newFileAction);
+    m_fileMenu->addAction(m_openFileAction);
     m_fileMenu->addAction(m_saveAsAction);
+
+    m_fileMenu->addSeparator();
+
+    m_fileMenu->addAction(m_exitAction);
+
+    // Edit menu
+    m_editMenu = menuBar()->addMenu("&Edit");
+    m_editMenu->addAction(m_copyAction);
+    m_editMenu->addAction(m_pasteAction);
+    m_editMenu->addAction(m_cutAction);
+
+    m_editMenu->addSeparator();
+
+    m_editMenu->addAction(m_undoAction);
+    m_editMenu->addAction(m_redoAction);
 }
 
-// Verifies with the user if it is okay to continue
-bool MainWindow::okayToContinue()
+// Creates a movable tool bar
+void MainWindow::createToolBars()
 {
+    // File tool bar
+    m_fileToolBar = addToolBar("&File");
+    m_fileToolBar->addAction(m_newFileAction);
+    m_fileToolBar->addAction(m_openFileAction);
+    m_fileToolBar->addAction(m_saveAsAction);
+
+    // Edit tool bar
+    m_editToolBar = addToolBar("&Edit");
+    m_editToolBar->addAction(m_copyAction);
+    m_editToolBar->addAction(m_pasteAction);
+    m_editToolBar->addAction(m_cutAction);
+
+    m_editToolBar->addSeparator();
+
+    m_editToolBar->addAction(m_undoAction);
+    m_editToolBar->addAction(m_redoAction);
+}
+
+// Creates a context menu when the user clicks the RMB
+void MainWindow::createContextMenu()
+{
+    m_textEdit->addAction(m_copyAction);
+    m_textEdit->addAction(m_pasteAction);
+    m_textEdit->addAction(m_cutAction);
+    m_textEdit->addAction(m_undoAction);
+    m_textEdit->addAction(m_redoAction);
+    m_textEdit->setContextMenuPolicy(Qt::ActionsContextMenu);
+}
+
+//TODO: Verifies with the user if it is okay to continue
+//bool MainWindow::okayToContinue()
+//{
     //if (isWindowModified())
         //{
             //int r = QMessageBox::warning(this, "Notepad", "The document has been modified.\n"
@@ -56,7 +160,7 @@ bool MainWindow::okayToContinue()
         //}
 
     //return true;
-}
+//}
 
 // Creates a new file TODO: implement okayToContinue() method
 void MainWindow::newFile()
@@ -71,13 +175,43 @@ void MainWindow::newFile()
     m_textEdit->setText(QString());
 }
 
+// Open a file from the users system
+void MainWindow::openFile()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+
+    // If unable to open file, return an error message to the user
+    if (!file.open(QIODevice::ReadOnly | QFile::Text))
+    {
+        statusBar()->showMessage("Cannot open file: " + file.errorString(), 5000);
+
+        return;
+    }
+
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    m_textEdit->setText(text);
+
+    file.close();
+}
+
+//TODO: Save updated contents to to current file
+//void MainWindow::saveFile()
+//{
+
+//}
+
 // Saves the file with a file name and path specified by the user
 void MainWindow::saveAs()
 {
     QString fileName = QFileDialog::getSaveFileName(this, "Save as");
     QFile file(fileName);
 
-    if (!file.open(QFile::WriteOnly) | QFile::Text)
+    // Display error if unable to write to file
+    if (!file.open(QFile::WriteOnly | QFile::Text))
     {
             statusBar()->showMessage("Cannot save file: " + file.errorString(), 5000);
 
@@ -91,4 +225,46 @@ void MainWindow::saveAs()
     out << text;
 
     file.close();
+}
+
+//Opens recently edited files
+//void MainWindow::openRecentFile()
+//{
+
+//}
+
+// Exits the application
+void MainWindow::exit()
+{
+    QApplication::quit();
+}
+
+// Copy selected text to clipboard
+void MainWindow::copyToClipboard()
+{
+    m_textEdit->copy();
+}
+
+// Paste text from clipboard to application
+void MainWindow::pasteFromClipboard()
+{
+    m_textEdit->paste();
+}
+
+// Cut selected text to clipboard
+void MainWindow::cutToClipboard()
+{
+    m_textEdit->cut();
+}
+
+// Undo previous action
+void MainWindow::undo()
+{
+    m_textEdit->undo();
+}
+
+// Redo previous action
+void MainWindow::redo()
+{
+    m_textEdit->redo();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,16 +4,19 @@
 #include <QMainWindow>
 
 #include <QTextEdit>
+#include <QTextStream>
 #include <QStatusBar>
-#include <QMenuBar>
+#include <QToolBar>
 #include <QMenu>
+#include <QMenuBar>
 #include <QFile>
 #include <QFileDialog>
-#include <QList>
-#include <QAction>
+#include <QApplication>
 #include <QMessageBox>
+#include <QList>
+#include <QIcon>
+#include <QAction>
 #include <QString>
-#include <QTextStream>
 #include <QIODevice>
 
 class MainWindow : public QMainWindow
@@ -26,18 +29,40 @@ public:
 
 private slots:
     void newFile();
+    void openFile();
     void saveAs();
+    void exit();
+    void copyToClipboard();
+    void pasteFromClipboard();
+    void cutToClipboard();
+    void undo();
+    void redo();
 
 private:
     void createActions();
     void createMenus();
-    bool okayToContinue();
+    void createToolBars();
+    void createContextMenu();
+    //bool okayToContinue();
 
     QTextEdit *m_textEdit;
+
     QAction *m_newFileAction;
+    QAction *m_openFileAction;
     QAction *m_saveAsAction;
+    QAction *m_exitAction;
+    QAction *m_copyAction;
+    QAction *m_pasteAction;
+    QAction *m_cutAction;
+    QAction *m_undoAction;
+    QAction *m_redoAction;
+
     QMenu *m_fileMenu;
     QMenu *m_editMenu;
+
+    QToolBar *m_fileToolBar;
+    QToolBar *m_editToolBar;
+
     QString currentFile;
 };
 
